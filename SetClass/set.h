2f8315af3f97e0dc2f527694db0a08310a488fd6
Bharat Singh Rajput <34980375+iambharatsingh@users.noreply.github.com>
2018-05-31 00:35:23 +0530

    Update set.h

--- SetClass/set.h
+++ SetClass/set.h
@@ -4,32 +4,29 @@
 #include <iostream>
 #include <list>
 #include <iterator>
+#include <assert.h>
 
 template <typename T>
 class Set {
 		std :: list <T> setList;
-		int length;
 	public :
 		Set(std :: initializer_list <T> l) {
 			setList = l;
 			setList.sort();
 			T elem = setList.front();
-			typename std :: list <T> :: iterator i;
-			bool found = true;
-			for(i = setList.begin() ; i != setList.end() ;) {
-				if(elem != *(i) || found) {
-					found = false;
+			typename std :: list <T> :: iterator i = setList.begin();
+			i++;
+			while (i != setList.end()) {
+				if(elem != *(i)) {
 					elem = *(i);
 					i++;
 				} else {
 					setList.erase(i++);
 				}
 			}
-			length = setList.size();
-		} 
+		}
 		
 		Set() {
-			length = 0;
 		}
 
 		void printSet() {
@@ -39,6 +36,39 @@ class Set {
 				std :: cout << " " << *(i);
 			}
 			std :: cout << " }";
+		}
+
+		void add(T elem) {
+
+			typename std :: list <T> :: iterator i ;
+			for(i = setList.begin() ; i != setList.end() ; i++) {
+				if(elem == *(i)) {
+					return;
+				}
+			}
+
+			setList.push_back(elem);
+		}
+
+		void remove(T elem) {
+			typename std :: list <T> :: iterator i ;
+			for(i = setList.begin() ; i != setList.end() ; i++) {
+				if(elem == *(i)) {
+					setList.erase(i++);
+					return;
+				}
+			}
+		}
+
+		T pop() {
+			assert(setList.size() > 0);
+			T elem = setList.front();
+			setList.pop_front();
+			return elem;
+		}
+
+		int getLength() {
+			return setList.size();
 		}
 
 
